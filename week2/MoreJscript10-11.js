4c18458e178f65c90034c60e07f4a4300ef8ecef
mohammad286 <abozamel286@gmail.com>
2018-02-24 16:39:08 +0100

    moreJavaScript part10 made plural

--- week2/MoreJscript10-11.js
+++ week2/MoreJscript10-11.js
@@ -7,6 +7,6 @@ let length = vehicles.length;
 let part1 = vehicles.slice(0, length - 1);//seperate vehicles array till last index bus
 let part2 = vehicles.slice(length - 1);// gets bus
 
-advertisement += part1.join(", ") + " and " + part2.join('') + ".";//till bus puts "," in between  for the last index bus puts "."
+advertisement += part1.join("s, ") + " and " + part2.join('es') + ".";//till bus puts "," in between  for the last index bus puts "."
 
 console.log(advertisement);
