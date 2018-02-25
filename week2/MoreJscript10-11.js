06e1ec79db0b2d7e101b0dfd737845b8f24fd929
MURATKAYMAZ56 <muratkaymaz56@gmail.com>
2018-02-25 12:01:36 +0100

    credantial problem solved

--- week2/MoreJscript10-11.js
+++ week2/MoreJscript10-11.js
@@ -4,9 +4,12 @@ let vehicles = ["motorbike", "caravan", "bike", "truck", "bus"];
 let advertisement = "Amazing Joe's Garage, we service ";
 
 let length = vehicles.length;
+for (let i = 0; i < vehicles.length; i++) {
+    vehicles[i] = vehicles[i] + 's';
+}
 let part1 = vehicles.slice(0, length - 1);//seperate vehicles array till last index bus
 let part2 = vehicles.slice(length - 1);// gets bus
 
-advertisement += part1.join("s, ") + " and " + part2.join('es') + ".";//till bus puts "," in between  for the last index bus puts "."
+advertisement += part1.join(", ") + " and " + part2.join('') + ".";//till bus puts "," in between  for the last index bus puts "."
 
 console.log(advertisement);
